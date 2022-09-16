9de1e6613e972bc908ace5e2176df7b6907935b3
Pavani A <113224200+Pavaniankireddypalli316@users.noreply.github.com>
2022-09-16 09:27:25 +0530

    test day 6

---
+++ Month.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Month {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int year = sc.nextInt();
+    int month = sc.nextInt();
+    System.out.println(display(year, month));
+  }
+  public static int display(int year, int month) {
+    Calendar cal = Calendar.getInstance();
+    cal.set(Calendar.YEAR, year);
+    cal.set(Calendar.MONTH, month);
+    int DAY_OF_MONTH = cal.getActualMaximum(cal.DAY_OF_MONTH);
+    return DAY_OF_MONTH;
+ }
+} 
